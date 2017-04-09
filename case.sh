70992357d55e61f83a802e97214c74728f25922a
Etsu <etsu4296@gmail.com>
2017-04-09 10:45:46 +0300

    Create case.sh

---
+++ case.sh
@@ -0,0 +1,9 @@
+students=$1
+case "$students" in
+0) echo "No students";;
+1) echo "1 student";;
+2) echo "2 students";;
+3) echo "3 students";;
+4) echo "4 students";;
+*) echo "A lot of students";;
+esac
