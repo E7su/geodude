3cf8b6bf4382743d7056cbd0caaec6c3990d1a4d
Etsu <etsu4296@gmail.com>
2017-05-25 15:44:09 +0300

    Create kafka_console_groupid_finder.sh

---
+++ server/kafka_console_groupid_finder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./bin/zookeeper-shell.sh localhost:2181 ls /consumers | tr ', ' ' \n' | tr '[' '\n' | tr ']' ' ' > tmp
+
+sleep 10
+
+./bin/zookeeper-shell.sh localhost:2181 ls /consumers | tr ', ' ' \n' | tr '[' '\n' | tr ']' ' ' > tmp2
+
+diff tmp tmp2 | tail -n 1 | tr '>' ' '
