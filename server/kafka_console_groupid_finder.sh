20dac59680c2dda51947707bb3b3df7a166a363b
Etsu <etsu4296@gmail.com>
2017-05-25 16:09:17 +0300

    fix tr

--- server/kafka_console_groupid_finder.sh
+++ server/kafka_console_groupid_finder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./bin/zookeeper-shell.sh localhost:2181 ls /consumers | tr ', ' ' \n' | tr '[' '\n' | tr ']' ' ' > tmp
+./bin/zookeeper-shell.sh localhost:2181 ls /consumers | tr ',' ' \n' | tr '[' '\n' | tr ']' ' ' > tmp
 
 sleep 10
 
-./bin/zookeeper-shell.sh localhost:2181 ls /consumers | tr ', ' ' \n' | tr '[' '\n' | tr ']' ' ' > tmp2
+./bin/zookeeper-shell.sh localhost:2181 ls /consumers | tr ',' ' \n' | tr '[' '\n' | tr ']' ' ' > tmp2
 
 diff tmp tmp2 | tail -n 1 | tr '>' ' '
