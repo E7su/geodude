0b43da06bd2399f6d7d1f85229c2b1bb6c01ebca
Etsu <etsu4296@gmail.com>
2017-06-27 17:43:15 +0300

    remove redundant circle

--- server/git_pusher.sh
+++ server/git_pusher.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-again=yes
 tmp_path=/tmp/zeppelin_script/latest_commit_tmsp
-current_latest_commit_tmsp=`git log -n1 --format="%at"`
 
-while [ "$again" == "yes" ]
-do
+# if last commit time on disk < current last commit time, then push:
+if [ `cat $tmp_path` -lt `git log -n1 --format="%at"` ]; then
   git push
 
   # if push was success or nothing to push:
   if [ $? -eq 0 ]; then
-
     # write latest commit timestamp on disc
     git log -n1 --format="%at" > $tmp_path
-
-    # if last commit time on disk < current last commite time, then push
-    # else - break
-    if [ `cat $tmp_path` -eq `git log -n1 --format="%at"` ]; then
-      let "again=false"
-    fi
-
   fi
-done
+
+fi
