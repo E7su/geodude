20e33372509dc67f3e63f21c53fd78c9d1b95707
Etsu <etsu4296@gmail.com>
2017-07-06 11:48:27 +0300

    Update extend_resize.sh

--- server/extend_resize.sh
+++ server/extend_resize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-df -h | grep tmp | grep vg | awk {'print $1'} | xargs -I {} lvextend -L +100G {}
-df -h | grep tmp | grep vg | awk {'print $1'} | xargs -I {} resize2fs {}
+df -h | grep tmp | grep vg | awk {'print $1'} | xargs -I {} lvextend -L +100G {}  # увеличит размер LV
+df -h | grep tmp | grep vg | awk {'print $1'} | xargs -I {} resize2fs {}  # увеличит размер FS под LV
