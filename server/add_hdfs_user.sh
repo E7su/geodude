9a2f8e5231179cf05d7c299f33dd2e483a57a113
Etsu <etsu4296@gmail.com>
2017-07-10 16:56:26 +0300

    add chown

--- server/add_hdfs_user.sh
+++ server/add_hdfs_user.sh
@@ -2,5 +2,16 @@
 
 login=$1
 comment=$2
+
+# add user
 useradd $login -c "$comment" -m  -b /team -g hdfs -N
 echo "useradd $login -c \"$comment\" -m -b /team -g hdfs -N"
+
+# create user's home
+hadoop fs –mkdir /user/$login
+echo "hadoop fs –mkdir /user/$login"
+
+# chown
+sudo su hdfs
+hadoop fs –chown –R $login:hdfs /team/$login
+echo "hadoop fs –chown –R $login:hdfs /team/$login"
