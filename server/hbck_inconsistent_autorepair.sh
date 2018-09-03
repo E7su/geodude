9210b86295d8cdde29f910298fad6ef41875b0a7
|K07e7su <etsu4296@gmail.com>
2018-09-03 16:16:58 +0300

    fix check

--- server/hbck_inconsistent_autorepair.sh
+++ server/hbck_inconsistent_autorepair.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+LOG=/tmp/repair.log
 CHECK=`su hbase -c '_JAVA_OPTIONS="-Xms128m -Xmx128m" hbase hbck' -s /bin/bash | grep -i INCONSISTENT`
-echo ">>> $CHECK" 2&>> /tmp/repair.log
+echo ">>> $CHECK" &>> $LOG
 
 if [[ -n $CHECK ]];
   then
   echo "fix-fix-fix"
-  su hbase -c '_JAVA_OPTIONS="-Xms128m -Xmx128m" hbase hbck -repair' -s /bin/bash 2&>> /tmp/repair.log
+  su hbase -c '_JAVA_OPTIONS="-Xms128m -Xmx128m" hbase hbck -repair' -s /bin/bash &>> $LOG
 fi
